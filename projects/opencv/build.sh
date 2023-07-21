a70f066636f8b08cf166ef035a88fcf028fecf8f
Vincent Rabaud <vrabaud@google.com>
2023-07-21 17:54:37 +0200

    Get OpenCV to use upstream fuzz targets.

--- projects/opencv/build.sh
+++ projects/opencv/build.sh
@@ -18,31 +18,48 @@
 build_dir=$WORK/build-$SANITIZER
 install_dir=$WORK/install-$SANITIZER
 
+compile_fuzztests.sh
+
+#rm -fr $build_dir
 mkdir -p $build_dir
 pushd $build_dir
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$install_dir \
-  -DBUILD_SHARED_LIBS=OFF -DOPENCV_GENERATE_PKGCONFIG=ON \
-  -DOPENCV_GENERATE_PKGCONFIG=ON -DOPENCV_FORCE_3RDPARTY_BUILD=ON \
-  -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_opencv_apps=OFF \
-  $SRC/opencv
-make -j$(nproc)
-make install
-popd
+# Force static because absl does not compile otherwise.
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$install_dir \
+  -DBUILD_SHARED_LIBS=OFF -DOPENCV_GENERATE_PKGCONFIG=OFF \
+  -DBUILD_TESTS=ON -DBUILD_FUZZ_TESTS=ON -DBUILD_PERF_TESTS=OFF \
+  -DBUILD_opencv_apps=OFF $SRC/opencv
+#  -DCMAKE_C_COMPILER="${CC}" -DCMAKE_CXX_COMPILER="${CXX}" \
+#  -DCMAKE_C_FLAGS="${CFLAGS}" \
+#  -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+#  -DCMAKE_EXE_LINKER_FLAGS="${CXXFLAGS}" \
+#  -DCMAKE_MODULE_LINKER_FLAGS="${CXXFLAGS}" \
+#  -DCMAKE_SHARED_LINKER_FLAGS="${CXXFLAGS}" \
+#  -DCMAKE_STATIC_LINKER_FLAGS="${CXXFLAGS}" $SRC/opencv
+make -j $(nproc) opencv_fuzz_tests
+# The following is taken from https://github.com/google/oss-fuzz/blob/31ac7244748ea7390015455fb034b1f4eda039d9/infra/base-images/base-builder/compile_fuzztests.sh#L59
+# Iterate the fuzz binaries and list each fuzz entrypoint in the binary. For
+# each entrypoint create a wrapper script that calls into the binaries the
+# given entrypoint as argument.
+# The scripts will be named:
+# {binary_name}@{fuzztest_entrypoint}
+FUZZ_TEST_BINARIES_OUT_PATHS=`ls ./bin/opencv_fuzz_*`
+#cp ./bin/opencv_fuzz_* $OUT/
+echo "Fuzz binaries: $FUZZ_TEST_BINARIES_OUT_PATHS"
+for fuzz_main_file in $FUZZ_TEST_BINARIES_OUT_PATHS; do
+  FUZZ_TESTS=$($fuzz_main_file --list_fuzz_tests | cut -d ' ' -f 4)
+  cp -f ${fuzz_main_file} $OUT/
+  fuzz_basename=$(basename $fuzz_main_file)
+  chmod -x $OUT/$fuzz_basename
+  for fuzz_entrypoint in $FUZZ_TESTS; do
+    TARGET_FUZZER="${fuzz_basename}@$fuzz_entrypoint"
 
-pushd $SRC
-for fuzzer in core_fuzzer filestorage_read_file_fuzzer \
-   filestorage_read_filename_fuzzer filestorage_read_string_fuzzer \
-   generateusergallerycollage_fuzzer imdecode_fuzzer imencode_fuzzer \
-   imread_fuzzer readnetfromtensorflow_fuzzer; do
-$CXX $CXXFLAGS $LIB_FUZZING_ENGINE $fuzzer.cc -std=c++11 \
--I$install_dir/include/opencv4 -L$install_dir/lib \
--L$install_dir/lib/opencv4/3rdparty \
--lopencv_dnn -lopencv_objdetect -lopencv_photo -lopencv_ml -lopencv_gapi \
--lopencv_stitching -lopencv_video -lopencv_calib3d -lopencv_features2d \
--lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_imgproc \
--lopencv_flann -lopencv_core -llibjpeg-turbo -llibwebp -llibpng -llibtiff \
--llibopenjp2 -lIlmImf -llibprotobuf -lquirc -lzlib -littnotify -lippiw \
--lippicv -lade -ldl -lm -lpthread -lrt \
--o $OUT/$fuzzer
+    # Write executer script
+    echo "#!/bin/sh
+# LLVMFuzzerTestOneInput for fuzzer detection.
+this_dir=\$(dirname \"\$0\")
+chmod +x \$this_dir/$fuzz_basename
+$fuzz_basename --fuzz=$fuzz_entrypoint -- \$@" > $OUT/$TARGET_FUZZER
+    chmod +x $OUT/$TARGET_FUZZER
+  done
 done
 popd
